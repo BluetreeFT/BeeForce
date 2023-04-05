34b16aecc6c4153a7eac03bc1e7ac66dda8c404a
BluetreeFT <sathish@bluetreemail.in>
2023-04-05 10:41:58 +0530

    5th april commit

--- src/test/java/test/beeforce/onboarding/pageobjects/STLBudgectingAndSourcing.java
+++ src/test/java/test/beeforce/onboarding/pageobjects/STLBudgectingAndSourcing.java
@@ -21,6 +21,48 @@ public class STLBudgectingAndSourcing extends BaseClass  {
    @FindBy(xpath="//label[contains(text(),'BU')]/following-sibling::div/select")
    WebElement businessUnit;
    
+   @FindBy(xpath="//label[contains(text(),'OU')]/following-sibling::div/select")
+   WebElement organizationUnit;
+   
+   @FindBy(xpath="//label[contains(text(),'Location')]/following-sibling::div/select")
+   WebElement location;
+   
+   @FindBy(xpath="//label[contains(text(),'Cost Center')]/following-sibling::div/select")
+   WebElement costCenter;
+   
+   @FindBy(xpath="//label[contains(text(),'Department')]/following-sibling::div/select")
+   WebElement department;
+   
+   @FindBy(xpath="//label[contains(text(),'Associate Type')]/following-sibling::div/select")
+   WebElement associateType;
+   
+   @FindBy(id="name")
+   WebElement name;
+   
+   @FindBy(id="comments")
+   WebElement comments;
+   
+   @FindBy(xpath="//button[contains(text(),'Generate')]")
+   WebElement btnGenerate;
+   
+   @FindBy(id="details0headCount")
+   WebElement headCount;
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    
    
    
